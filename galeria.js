1571cda58d4668df60f834a8d67a62a0cfd028fd
Cauã Menezes <cauamenezes.07@gmail.com>
2021-09-30 21:23:01 -0300

    first commit

---
+++ galeria.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const imagens = [
+  "./img/seca.jpg",
+  "./img/tempestade-de-areia.jpg",
+  "./img/incendio-florestal.jpg",
+  "./img/furacao.jpg",
+  "./img/erupcao.jpg",
+  "./img/tsunami.jpg",
+  "./img/terremoto.jpg"
+];
+
+// MAP - FILTER - REDUCE -> Declarativa
+
+const criarItem = (urlImagem) => {
+  const container = document.querySelector(".galeria-container");
+
+  const novoLink = document.createElement("a");
+  novoLink.href = urlImagem;
+  novoLink.classList.add("galeria-items");
+  novoLink.innerHTML = `<img src="${urlImagem}" alt="mario"></img>`;
+
+  container.appendChild(novoLink);
+
+  //   container.innerHTML += `
+  //     <a href="${urlImagem}" class="galeria-items">
+  //         <img src="${urlImagem}" alt="mario">
+  //     </a>
+  //     `;
+};
+
+const carregarImagens = () => imagens.forEach(criarItem);
+
+carregarImagens();
