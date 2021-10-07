5daeb14c2c3427a95f52f19b11da8aec175a62ff
Cauã Menezes <cauamenezes.07@gmail.com>
2021-10-07 20:44:00 -0300

    slides entre as imagens adicionados

--- galeria.js
+++ galeria.js
@@ -10,25 +10,47 @@ const imagens = [
   "./img/terremoto.jpg",
 ];
 
-// MAP - FILTER - REDUCE -> Declarativa
+const limparId = (url) =>
+  url.replace("./img/", "").split(".")[0].replace(" ", "-");
 
 const criarItem = (urlImagem) => {
   const container = document.querySelector(".galeria-container");
 
   const novoLink = document.createElement("a");
-  novoLink.href = urlImagem;
+  novoLink.href = `#${limparId(urlImagem)}`;
   novoLink.classList.add("galeria-items");
   novoLink.innerHTML = `<img src="${urlImagem}" alt="mario"></img>`;
 
   container.appendChild(novoLink);
-
-  //   container.innerHTML += `
-  //     <a href="${urlImagem}" class="galeria-items">
-  //         <img src="${urlImagem}" alt="mario">
-  //     </a>
-  //     `;
 };
 
-const carregarImagens = () => imagens.forEach(criarItem);
+const carregarGaleria = (imagens) => imagens.forEach(criarItem);
 
-carregarImagens();
+const criarSlide = (urlImagem, indice, arr) => {
+  const container = document.querySelector(".slide-container");
+  const novoDiv = document.createElement("div");
+  novoDiv.classList.add("slide");
+  novoDiv.id = limparId(urlImagem);
+
+  const indiceAnterior = indice <= 0 ? arr.length - 1 : indice - 1;
+  const idAnterior = limparId(arr[indiceAnterior]);
+
+  const indicePosterior = indice >= arr.length - 1 ? 0 : indice + 1;
+  const idPosterior = limparId(arr[indicePosterior]);
+
+  novoDiv.innerHTML = `
+        <div class="imagem-container">
+          <a href="" class="icones fechar">&#10006;</a>
+          <a href="#${idAnterior}" class="icones anterior">&#171;</a>
+          <img src="${urlImagem}" alt="" />
+          <a href="#${idPosterior}" class="icones proximo">&#187;</a>
+        </div>
+  `;
+
+  container.appendChild(novoDiv);
+};
+
+const carregarSlide = (imagens) => imagens.forEach(criarSlide);
+
+carregarGaleria(imagens);
+carregarSlide(imagens);
